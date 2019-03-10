00616bef98aa5a226c54445458bc7fe194ae5878
KatieJessupMcD <katie.jessupmcd@gmail.com>
2019-03-09 17:35:20 -0800

    basic boilerplate, routes commented out

---
+++ app.js
@@ -0,0 +1,32 @@
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+// app.get("/jokes", async function(req, res, next) {
+//   try {
+//     const jokes = await Joke.getRandomJokes();
+//     return res.json({ jokes });
+//   } catch (e) {
+//     return next(e);
+//   }
+// });
+
+// app.get("/jokes/best", async function(req, res, next) {
+//   try {
+//     const jokes = await Joke.getTopJokes();
+//     return res.json({ jokes });
+//   } catch (e) {
+//     return next(e);
+//   }
+// });
+
+// app.get("/jokes/worst", async function(req, res, next) {
+//   try {
+//     const jokes = await Joke.getBottomJokes();
+//     return res.json({ jokes });
+//   } catch (e) {
+//     return next(e);
+//   }
+// });
+
+app.listen(3001, () => console.log("Server started on 3001"));

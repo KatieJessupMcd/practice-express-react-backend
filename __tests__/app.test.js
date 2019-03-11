76ff7d03c78a215794e9f5971ed1c354082659dc
KatieJessupMcD <katie.jessupmcd@gmail.com>
2019-03-11 16:50:53 -0700

    added test examples

---
+++ __tests__/app.test.js
@@ -0,0 +1,29 @@
+// UNIT TESTING IN JEST
+// npm test -- --watch to watch and re-run tests as test files change
+// ie 
+/*
+describe("calculateCartTotal", function() {
+  // let's make a variable cart to be used in each test
+  let cart;
+
+  beforeEach(function() {
+    cart = [
+      { item: "le croix", price: 4.99, quantity: 3 },
+      { item: "pretzels", price: 8.99, quantity: 10 }
+    ];
+  });
+  test("it calculates total without discount", function() {
+    const total = calculateCartTotal(cart);
+    expect(total).toBe(104.87);
+  });
+  test("it calculates total with discount", function() {
+    const total = calculateCartTotal(cart, 0.5);
+    expect(total).toBe(52.44);
+  });
+  test("it handles empty carts", function() {
+    const total = calculateCartTotal([]);
+    expect(total).toBe(0);
+  });
+});
+*/
+
---
+++ __tests__/joke.test.js
@@ -0,0 +1,14 @@
+// Testing CRUD actions
+/** GET /cats - returns `{cats: [cat, ...]}` */
+/*
+
+describe("GET /cats", async function() {
+  test("Gets a list of 1 cat", async function() {
+    const response = await request(app).get(`/cats`);
+    const { cats } = response.body;
+    expect(response.statusCode).toBe(200);
+    expect(cats).toHaveLength(1);
+    expect(cats[0]).toEqual(cat);
+  });
+});
+*/

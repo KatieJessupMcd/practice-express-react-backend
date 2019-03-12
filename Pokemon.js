3d4b3ccc14c1f57188b0702e9b574e7f5931264e
KatieJessupMcD <katie.jessupmcd@gmail.com>
2019-03-12 11:54:53 -0700

    renamed jokes to pokemon

---
+++ Pokemon.js
@@ -0,0 +1,30 @@
+// MODEL
+// exports Model
+const axios = require("axios");
+const db = require("./db");
+const BASE_URL = "https://pokeapi.co/api/v2/pokemon/";
+
+class Pokemon {
+  static async getPokemon(){
+    let newPokemonRes = await axios.get(BASE_URL);
+    let newPokemonData = newPokemonRes.data.results; 
+    return newPokemonData; 
+  }
+  
+
+  static async insert({ id, joke }) {
+    try {
+
+      const newJoke = await db.query(`
+        INSERT INTO jokes (id, joke) VALUES ($1, $2) RETURNING *;
+      `, [id, joke]);
+      return newJoke.rows[0];
+    } catch (e) {
+      throw new Error("oops", e)
+    }
+  }
+
+  
+}
+
+module.exports = Pokemon

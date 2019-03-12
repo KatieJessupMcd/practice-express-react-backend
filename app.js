6b1766e75834b7cafd2758e5b0b549d4474c2817
KatieJessupMcD <katie.jessupmcd@gmail.com>
2019-03-12 11:54:23 -0700

    changed routes

--- app.js
+++ app.js
@@ -1,34 +1,20 @@
 // ROUTES
 // starts server, route handlers that use class methods defined in MODEL
+const express = require('express'); 
+const cors = require('cors'); 
 const app = express();
+const Pokemon = require('./Pokemon'); 
 app.use(cors());
 app.use(express.json());
 
-app.get("/jokes", async function(req, res, next) {
+app.get("/pokemon", async function(req, res, next) {
   try {
-    const jokes = await Joke.getRandomJokes();
-    return res.json({ jokes });
+    const pokemonList = await Pokemon.getPokemon();
+    return res.json({ pokemonList });
   } catch (e) {
     return next(e);
   }
 });
 
-// app.get("/jokes/best", async function(req, res, next) {
-//   try {
-//     const jokes = await Joke.getTopJokes();
-//     return res.json({ jokes });
-//   } catch (e) {
-//     return next(e);
-//   }
-// });
-
-// app.get("/jokes/worst", async function(req, res, next) {
-//   try {
-//     const jokes = await Joke.getBottomJokes();
-//     return res.json({ jokes });
-//   } catch (e) {
-//     return next(e);
-//   }
-// });
 
 app.listen(3001, () => console.log("Server started on 3001"));
